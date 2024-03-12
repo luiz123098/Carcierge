b2279247a513b3fe367572dc2814f29e851eb9a4
Neto <luiznetopaluti@hotmail.com>
2024-03-12 20:27:32 -0300

    First commit

---
+++ src/main/java/com/carcierge/demo/CarciergeApiApplication.java
@@ -0,0 +1,13 @@
+package com.carcierge.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarciergeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarciergeApiApplication.class, args);
+	}
+
+}
